26fd90e5a390cefaee0520111996d2fe69a3c067
okuc <okuc@live.cn>
2020-05-10 19:16:16 +0800

    获取屏幕分辨率及鼠标位置

--- src/compments/ZoomPage.tsx
+++ src/compments/ZoomPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Slider } from "antd";
 
 import { remote, webFrame } from "electron";
 
@@ -11,13 +10,33 @@ const ZoomPage = () => {
     const zoomIn = () => {
         webFrame.setZoomLevel(webFrame.getZoomLevel()-1);
       };
-        
+      const maxScreen = () => {
+            //获得屏幕的高度和宽度
+          const {width,height} = remote.screen.getPrimaryDisplay().workAreaSize;
+
+          remote.getCurrentWindow().setSize(width,height);
+          remote.getCurrentWindow().setPosition(0,0);
+        };
+        const getInfo = () => {
+              //获得屏幕的高度和宽度
+            const {width,height} = remote.screen.getPrimaryDisplay().workAreaSize;
+            setScreen(()=>({width:width,height:height}));
+            //获取鼠标位置
+            const point = remote.screen.getCursorScreenPoint();
+            setCursor(()=>({x:point.x,y:point.y}));
+          };
+        const [screen, setScreen] = useState({width:0,height:0});//保存屏幕的长和宽
+        const [cursor, setCursor] = useState({x:0,y:0});//保存鼠标位置
 
   return (
     <div>
       <div>这里是页面内容</div>
       <button onClick={zoomOut}>放大页面</button>
       <button onClick={zoomIn}>缩小页面</button>
+      <button onClick={maxScreen}>窗口占满整个屏幕</button>
+      <button onClick={getInfo}>获取屏幕分辨率及鼠标位置</button>
+  <div>屏幕分辨率：{screen.width}--{screen.height}</div>
+  <div>鼠标位置：{cursor.x}-----{cursor.y}</div>
    </div>
   );
 };
